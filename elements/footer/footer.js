0d3de4a51754fcf998e036e1b6fdd72f005ef0cf
desireex <64332786+desireex@users.noreply.github.com>
2025-01-27 17:36:44 -0300

    Delete elements/footer directory

--- elements/footer/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("elements/footer/footer.html")
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("footer-placeholder").innerHTML = data;
-      })
-      .catch(error => console.error("Erro ao carregar o footer:", error));
-  });
-  
