6c6aa12d561810a0f7cf2d6d7a29a4b4d6e4ae27
desireex <64332786+desireex@users.noreply.github.com>
2025-01-27 17:35:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ if( id =="index"){
 footer.innerHTML= `
     <div id="footer_todo">
 
-        <link rel="stylesheet" href="${urls}footer.css">
+        <link rel="stylesheet" href="${urls}style.css">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
